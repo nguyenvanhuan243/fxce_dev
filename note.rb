95a3bcd95113d238d2978be808b703b4bcc7a990
Huan Nguyen <huannguyen@freec.asia>
2023-04-22 12:13:17 +0700

    add note

--- note.rb
+++ note.rb
@@ -5,3 +5,10 @@ page_admin: "https://fxce.onrender.com/blog/login"
 email: "fxce.phi@fxce.com"
 pass: "FXCE@DEV2023"
 "https://fxce.onrender.com/blog/login"
+
+email: "fxce.phong@fxce.com"
+pass: "FXCE@DEV2023"
+web: https://fxce.onrender.com/blog/login
+
+
+How to add new user: # rake lines:add_user
