2ef78b2e9d9dbceab5dcc590ff5c854f09be4cc4
Huan Nguyen <huannguyen@freec.asia>
2023-04-22 11:37:00 +0700

    add

--- note.rb
+++ note.rb
@@ -1,3 +1,7 @@
 email: "admin@fxce.com"
 pass: "FXCE@DEV2023"
 page_admin: "https://fxce.onrender.com/blog/login"
+
+email: "fxce.phi@fxce.com"
+pass: "FXCE@DEV2023"
+"https://fxce.onrender.com/blog/login"
